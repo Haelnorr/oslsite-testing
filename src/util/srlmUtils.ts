ae943fdf07c59c0ad92bcac06faf187bd4d80d4e
Haelnorr <lockedonohoe@gmail.com>
2024-04-11 11:12:54 +1000

    Added POST and BasicAuth functions to srlmUtils Documented functions in srlmUtils Added error handler for request functions Added more type definitions in srlmUtils Fixed some error handling on league/index.astro

--- src/util/srlmUtils.ts
+++ src/util/srlmUtils.ts
@@ -1,20 +1,40 @@
 import axios from "axios";
+import type { StringLiteral } from "typescript";
 
 const api = {
     uri: import.meta.env.SRLM_API_URI,
     key: import.meta.env.SRLM_API_APP_KEY
 };
 
-// makes a request to the SRLM API
+// handles response errors from the SRLM API
+// will log the error message from SRLM API to the console along with the headers of the failed request
+// returns null
+function handle_err(err) {
+    if (err) {
+        console.log(err.response.data['message']);
+        console.log(err.request._header)
+        return null
+    }
+}
+
+/** 
+ * Makes a GET request to the SRLM api.
+ * Uses axios and environment variables to make a GET request to the SRLM api. If request fails, returns null and logs the API's error message and the request headers to console.
+ * 
+ * @param {string} endpoint       endpoint for the request - appends to env var SRLM_API_URI
+ * @param {string} [user_token]   user auth token - appends to SRLM_API_APP_KEY
+ * 
+ * @returns response.data
+ */
 export async function srlm_get(endpoint:string, user_token:string='') {
     const request_url = api['uri'] + endpoint;
     const api_token = api['key'] + user_token;
 
     return await axios.get(request_url, {
-    headers: {
-        Authorization: `Bearer ${api_token}`
-    }
-    }).then((response) => response.data).catch((err) => err);
+        headers: {
+            Authorization: `Bearer ${api_token}`
+        }
+    }).then((response) => response.data).catch((err) => handle_err(err));
 };
 
 // strip a link from the SRLM API of its /api prefix, allowing it to be used for url routing
@@ -22,20 +42,92 @@ export function convert_link(link: string) {
     return link.replace('/api', '')
 }
 
-export type Collection = {
-    items: [];
+/** 
+ * Makes a POST request to the SRLM api.
+ * Uses axios and environment variables to make a POST request to the SRLM api. If request fails, returns null and logs the API's error message and the request headers to console.
+ * 
+ * @param {string} endpoint       endpoint for the request - appends to env var SRLM_API_URI
+ * @param {string} [user_token]   user auth token - appends to SRLM_API_APP_KEY
+ * @param {dict}   [input]        body of the post request
+ * 
+ * @returns response.data
+ */
+export async function srlm_post(endpoint: string, user_token:string='', input:{}={}) {
+    const request_url = api['uri'] + endpoint;
+    const api_token = api['key'] + user_token;
+
+    return await axios.post(request_url, input, {
+        headers: {
+            Authorization: `Bearer ${api_token}`
+        }
+    }).then((response) => response.data).catch((err) => handle_err(err));
+}
+
+/** 
+ * Makes a Basic Auth POST request to the SRLM api.
+ * Uses axios and environment variables to make a basic auth request to the SRLM api. If request fails, returns null and logs the API's error message and the request headers to console.
+ * 
+ * @param {string} username   username of the user
+ * @param {string} password   password of the user
+ * 
+ * @returns response.data
+ */
+export async function srlm_basic_auth(username: string, password: string) {
+    const request_url = api['uri'] + '/auth/user';
+    
+    return await axios.post(request_url, {}, {
+        auth: {
+            username: username,
+            password: password
+        }
+    }).then((response) => response.data).catch((err) => handle_err(err));
+}
+
+export type Token = {
+    token: string;
+    expires: Date;
+}
+
+export type User = {
+    id: number;
+    username: string;
+    email: string;
+    player: number;
+    discord: number;
+    permissions: Array<string>;
+    matches_streamed: number;
+    reset_pass: boolean;
     _links: {
         self: string;
-        next: string;
-        prev: string
-    };
-    _meta: {
-        page: number;
-        per_page: number;
-        total_pages: number;
-        total_items: number;
-    };
+        player: string;
+        discord: string;
+        permissions: string;
+        matches_streamed: string;
+    }
 }
+
+export type Permission = {
+    id: number;
+    key: string;
+    description: string;
+    users_count: number;
+    _links: {
+        self: string;
+    }
+}
+
+export type Discord = {
+    user: string;
+    discord_id: string;
+    access_token: string;
+    refresh_token: string;
+    token_expiration: Date;
+    _links: {
+        self: string;
+        user: string;
+    }
+}
+
 
 export type League = {
     id: number;
@@ -67,4 +159,56 @@ export type Season = {
         match_type: string;
         divisions: string;
     };
+}
+
+export type SeasonCollection = {
+    items: Array<Season>;
+    _links: {
+        self: string;
+        next: string;
+        prev: string
+    };
+    _meta: {
+        page: number;
+        per_page: number;
+        total_pages: number;
+        total_items: number;
+    };
+}
+
+export type Division = {
+    id: number;
+    name: string;
+    acronym: string;
+    league: string;
+    description: string;
+    seasons_count: number;
+    _links: {
+        self: string;
+        league: string;
+        seasons: string;
+    }
+}
+
+export type SeasonDivision = {
+    id: number;
+    season: string;
+    division: string;
+    league: string;
+    teams_count: number;
+    free_agents_count: number;
+    rookies_count: number;
+    matches_count: number;
+    finals_count: number;
+    _links: {
+        self: string;
+        league: string;
+        season: string;
+        division: string;
+        teams: string;
+        free_agents: string;
+        rookies: string;
+        matches: string;
+        finals: string;
+    }
 }

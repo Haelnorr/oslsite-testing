087176249e99b6a7333664ab5fa86e6b0ea6bb99
Haelnorr <lockedonohoe@gmail.com>
2024-04-12 20:51:44 +1000

    Moved type definitions to srlmTypes Updated the [season_id] page Updated the [season]/[division] page

---
+++ src/util/srlmTypes.ts
@@ -0,0 +1,135 @@
+export type Token = {
+    token: string;
+    expires: Date;
+}
+
+export type User = {
+    id: number;
+    username: string;
+    email: string;
+    player: number;
+    discord: number;
+    permissions: Array<string>;
+    matches_streamed: number;
+    reset_pass: boolean;
+    _links: {
+        self: string;
+        player: string;
+        discord: string;
+        permissions: string;
+        matches_streamed: string;
+    }
+}
+
+export type Permission = {
+    id: number;
+    key: string;
+    description: string;
+    users_count: number;
+    _links: {
+        self: string;
+    }
+}
+
+export type Discord = {
+    user: string;
+    discord_id: string;
+    access_token: string;
+    refresh_token: string;
+    token_expiration: Date;
+    _links: {
+        self: string;
+        user: string;
+    }
+}
+
+
+export type League = {
+    id: number;
+    name: string;
+    acronym: string;
+    seasons_count: number;
+    divisions_count: number;
+    _links: {
+        self: string;
+        seasons: string;
+        divisions: string;
+    };
+}
+
+type DivisionLink = {
+    name: string;
+    _link: string;
+}
+
+
+export type Season = {
+    id: number;
+    name: string;
+    acronym: string;
+    league: string;
+    start_date: Date;
+    end_date: Date;
+    finals_start: Date;
+    finals_end: Date;
+    match_type: string;
+    divisions: Array<DivisionLink>
+    _links: {
+        self: string;
+        league: string;
+        match_type: string;
+        divisions: string;
+    };
+}
+
+export type SeasonCollection = {
+    items: Array<Season>;
+    _links: {
+        self: string;
+        next: string;
+        prev: string
+    };
+    _meta: {
+        page: number;
+        per_page: number;
+        total_pages: number;
+        total_items: number;
+    };
+}
+
+export type Division = {
+    id: number;
+    name: string;
+    acronym: string;
+    league: string;
+    description: string;
+    seasons_count: number;
+    _links: {
+        self: string;
+        league: string;
+        seasons: string;
+    }
+}
+
+export type SeasonDivision = {
+    id: number;
+    season: Season;
+    division: Division;
+    league: string;
+    teams_count: number;
+    free_agents_count: number;
+    rookies_count: number;
+    matches_count: number;
+    finals_count: number;
+    _links: {
+        self: string;
+        league: string;
+        season: string;
+        division: string;
+        teams: string;
+        free_agents: string;
+        rookies: string;
+        matches: string;
+        finals: string;
+    }
+}
--- src/util/srlmUtils.ts
+++ src/util/srlmUtils.ts
@@ -1,5 +1,4 @@
 import axios from "axios";
-import type { StringLiteral } from "typescript";
 
 const api = {
     uri: import.meta.env.SRLM_API_URI,
@@ -87,147 +86,16 @@ export async function srlm_basic_auth(username: string, password: string) {
     }).then((response) => response.data).catch((err) => handle_err(err));
 }
 
-export type Token = {
-    token: string;
-    expires: Date;
+
+function display_date(date_str: string) {
+    var date = new Date(date_str)
+    return date.toDateString();
 }
 
-export type User = {
-    id: number;
-    username: string;
-    email: string;
-    player: number;
-    discord: number;
-    permissions: Array<string>;
-    matches_streamed: number;
-    reset_pass: boolean;
-    _links: {
-        self: string;
-        player: string;
-        discord: string;
-        permissions: string;
-        matches_streamed: string;
-    }
-}
-
-export type Permission = {
-    id: number;
-    key: string;
-    description: string;
-    users_count: number;
-    _links: {
-        self: string;
-    }
-}
-
-export type Discord = {
-    user: string;
-    discord_id: string;
-    access_token: string;
-    refresh_token: string;
-    token_expiration: Date;
-    _links: {
-        self: string;
-        user: string;
-    }
-}
-
-
-export type League = {
-    id: number;
-    name: string;
-    acronym: string;
-    seasons_count: number;
-    divisions_count: number;
-    _links: {
-        self: string;
-        seasons: string;
-        divisions: string;
-    };
-}
-
-type DivisionLink = {
-    name: string;
-    _link: string;
-}
-
-
-export type Season = {
-    id: number;
-    name: string;
-    acronym: string;
-    league: string;
-    start_date: Date;
-    end_date: Date;
-    finals_start: Date;
-    finals_end: Date;
-    match_type: string;
-    divisions: Array<DivisionLink>
-    _links: {
-        self: string;
-        league: string;
-        match_type: string;
-        divisions: string;
-    };
-}
-
-export type SeasonCollection = {
-    items: Array<Season>;
-    _links: {
-        self: string;
-        next: string;
-        prev: string
-    };
-    _meta: {
-        page: number;
-        per_page: number;
-        total_pages: number;
-        total_items: number;
-    };
-}
-
-export type Division = {
-    id: number;
-    name: string;
-    acronym: string;
-    league: string;
-    description: string;
-    seasons_count: number;
-    _links: {
-        self: string;
-        league: string;
-        seasons: string;
-    }
-}
-
-export type SeasonDivision = {
-    id: number;
-    season: Season;
-    division: Division;
-    league: string;
-    teams_count: number;
-    free_agents_count: number;
-    rookies_count: number;
-    matches_count: number;
-    finals_count: number;
-    _links: {
-        self: string;
-        league: string;
-        season: string;
-        division: string;
-        teams: string;
-        free_agents: string;
-        rookies: string;
-        matches: string;
-        finals: string;
-    }
-}
-
-export type SimpleTeam = {
-    name: string;
-    acronym: string;
-    color: string;
-    _links: {
-        self: string;
+export function date_is_none(date_field: any) {
+    if (date_field) {
+        return display_date(date_field);
+    } else {
+        return '';
     }
 }
